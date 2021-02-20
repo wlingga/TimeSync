fb23b8fc7ffcf339f6aabd7f057cf76a650a8e63
Seung Jae Yang <sjyang1126@gmail.com>
2021-02-20 12:03:20 -0500

    stuff added

--- TimeSync.py
+++ TimeSync.py
@@ -1,8 +1,19 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def hello():
-    return "Hello World!"
+    return render_template('home.html')
+
+@app.route("/login")
+
+def login():
+    return render_template('login.hmtl')
+
+@app.route("/profile")
+
+def profile():
+    return render_template('profile.html')
